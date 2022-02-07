984faa0714474f9b16d4873c77e9d4db52c7d0a1
butter-chicken27 <agraj.srivastava60@gmail.com>
2022-02-07 13:00:37 +0530

    Setup basic text file exchange between client and server

--- src/client.py
+++ src/client.py
@@ -4,8 +4,10 @@ from rdt_socket import rdt_recv_socket
   # pass
 
 PORT_NUMBER = 8080
+data = 'dummy'
 recv_socket = rdt_recv_socket('localhost', PORT_NUMBER)
 print('Listening for message at port', PORT_NUMBER)
-data, header_fields = recv_socket.recv()
-print(header_fields)
-print(data)
+while data != '\n':
+  data, header_fields = recv_socket.recv()
+  # print(header_fields)
+  print(data)
--- src/server.py
+++ src/server.py
@@ -1,9 +1,20 @@
 from rdt_socket import rdt_send_socket
+import os
 
+DATA_SIZE = 128
+data_file = open('data.txt', 'r')
+text = data_file.read()
+data_file.close()
+bytes_remaining = len(text)
 # def send_file():
   # pass
 PORT_NUMBER = 8080
 send_socket = rdt_send_socket()
-data = 'hello'
-send_socket.send(data, 'localhost', PORT_NUMBER)
-print('Data sent')
+while bytes_remaining > 0:
+  chunk_size = min(DATA_SIZE, bytes_remaining)
+  data = text[:chunk_size]
+  text = text[chunk_size:]
+  bytes_remaining -= chunk_size
+  send_socket.send(data, 'localhost', PORT_NUMBER)
+  print('Data sent')
+send_socket.send('\n', 'localhost', PORT_NUMBER)
