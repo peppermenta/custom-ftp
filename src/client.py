e66810633d1f41c6b92618b848bb2be2ef3f6722
boredtea <es19btech11017@iith.ac.in>
2022-02-14 20:00:38 +0530

    Fix tortor

--- src/client.py
+++ src/client.py
@@ -1,8 +1,12 @@
 from struct import pack
 from rdt_socket import rdt_socket
+import os
 
 # def recv_file():
   # pass
+
+HOME_DIR = os.path.join(os.path.dirname(__file__),'../')
+received_file = open(os.path.join(HOME_DIR,'data','received.txt'), 'wb')
 
 
 
@@ -11,20 +15,6 @@ SER_PORT_NUMBER = 8118
 data = 'dummy'
 count = 0
 recv_socket = rdt_socket('localhost', REC_PORT_NUMBER, 2.0)
-# print('Listening for message at port', REC_PORT_NUMBER)
-# while data != '\n':
-#   try:
-#     data, header_fields = recv_socket.recv()
-#   except:
-#     count += 1
-#     if count == 10:
-#       break
-#     print('No data')
-#     continue
-#   count = 0
-#   print(header_fields)
-#   print(data)
-# recv_socket.udp_socket.close()
 total_packets = 0
 transmission_rate = 0
 
@@ -43,30 +33,13 @@ while True:
 packet_trackers = [i for i in range(total_packets)]
 data_received = ['' for i in range(total_packets)]
 
-# def get_interval():
-#   # return interval indices start and end inclusive, comma separated in a string
-#   interval = f""
-#   for i in range(transmission_rate):
-#     if not packet_trackers[i]+1==packet_trackers[i+1]:
-#       interval += f"{packet_trackers[i]},{packet_trackers[i]}"
-#     else:
-#       interval += f",{packet_trackers[i]}"
-#       while i<transmission_rate:
-#         if packet_trackers[i+1]+1==packet_trackers[i+2]:
-#           i += 1
-#         else:
-#           break
-#       interval += f",{packet_trackers[i+1]}"
-
-#   return interval
 
 def get_interval():
   temp =  [str(element) for element in packet_trackers]
-  if len(packet_trackers) < 5:
+  if len(packet_trackers) < transmission_rate:
     return ",".join(temp)
-  return ",".join(temp[0:5])
+  return ",".join(temp[0:transmission_rate])
 
-print(get_interval())
 
 def process_data(data, packet_num):
   # delete packet_num element from packet_trackers and update data_received with data
@@ -80,14 +53,17 @@ while len(packet_trackers) > 0:
   while True:
     try:
       data, header_fields = recv_socket.recv()
-      print(header_fields)
       if(header_fields['mode'] == 0):
         continue
       packet_number = header_fields['packet_num']
-      print(packet_number, len(packet_trackers))
       process_data(data, packet_number)
-      # print(packet_number, len(packet_trackers))
     except:
       break
 
 recv_socket.udp_socket.close()
+
+data_received = "".join(data_received)
+
+# save the received data to a file
+received_file.write(str.encode(data_received))
+received_file.close()
